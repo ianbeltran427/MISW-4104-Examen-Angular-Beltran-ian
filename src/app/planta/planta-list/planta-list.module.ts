cebe147ec6e65106db18f2b01d862c3e19fbb91a
ianbeltran427 <ip.beltran@uniandes.edu.co>
2024-04-25 15:05:35 -0500

    Agrega primera versión donde agrega el modulo de planta, la clase sy su servicio, ya se lista en la pantalla, aun no tiene estlios ni pruebas ni el total

---
+++ src/app/planta/planta-list/planta-list.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PlantaListComponent } from './planta-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [PlantaListComponent]
+})
+export class PlantaListModule { }
---
+++ src/app/planta/planta.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { Planta } from './planta';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlantaService {
+
+  private apiUrl: string = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getPlantas(): Observable<Planta[]> {
+    return this.http.get<Planta[]>(this.apiUrl);
+  }
+
+}
