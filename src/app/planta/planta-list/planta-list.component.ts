13fa2936a07d2e7c88c8e416c1f8ffe4dff4ffd7
ianbeltran427 <ip.beltran@uniandes.edu.co>
2024-04-25 22:19:32 -0500

    Agrega algunos estilos y agrega consolidado de tipo

--- src/app/planta/planta-list/planta-list.component.ts
+++ src/app/planta/planta-list/planta-list.component.ts
@@ -10,11 +10,35 @@ import { PlantaService } from '../planta.service';
 export class PlantaListComponent implements OnInit {
 
   plantas: Array<Planta> = [];
+  conteoPlantasTipo: {
+    interior: number;
+    exterior: number;
+  } = {
+      interior: 0,
+      exterior: 0
+    };
+
   constructor(private plantaService: PlantaService) { }
 
   getPlantas(): void {
     this.plantaService.getPlantas().subscribe((plantas) => {
       this.plantas = plantas;
+      this.contarPlantasTipo(plantas);
+    });
+  }
+
+  contarPlantasTipo(plantas: Planta[]): void {
+    this.conteoPlantasTipo = {
+      interior: 0,
+      exterior: 0
+    };
+
+    plantas.forEach((planta) => {
+      if (planta.tipo === 'Interior') {
+        this.conteoPlantasTipo.interior++;
+      } else if (planta.tipo === 'Exterior') {
+        this.conteoPlantasTipo.exterior++;
+      }
     });
   }
 
